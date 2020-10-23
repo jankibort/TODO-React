3af781aed1629b24841dcad16044cdd4901a36a2
jankibort <jan.kibort@gmail.com>
2020-10-23 18:54:46 +0200

    made weather widget works

--- src/Components/Sidebar/Weather.js
+++ src/Components/Sidebar/Weather.js
@@ -2,33 +2,27 @@ import React from 'react';
 
 class Weather extends React.Component {
     state = {
-        weather: ''
+        todaysForecast: null,
+        city: null
     }
 
-    // async componentDidMount() {
-    //     try {
-    //         let result = await fetch('https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4d' +
-    //             'c2-a171-a23eec8f2172/resource/d3e96eb6-25ad-4d6c-8651-b1eb39155945/download/stopsingdansk.json');
-    //         let json = await result.json();
-    //         console.log(json);
-    //     } catch { }
+    async componentDidMount() {
+        const url = " https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/523920";
 
+        fetch(url).then((res) => { return res.json(); }).then((data) => {
+            const weather = data;
+            this.setState({
+                todaysForecast: weather.consolidated_weather[0].weather_state_name,
+                city: weather.title
+            });
+        });
 
-    //     // const url = "https://www.metaweather.com/api/location/523920"
-    //     // const response = await fetch(url, {
-    //     //     method: "GET",
-    //     //     headers: {
-    //     //         "Access-Control-Allow-Origin": "*"
-    //     //     }
-    //     // });
-    //     // const jsonData = await response.json();
-    //     // this.setState({ weather: jsonData.consolidated_weather[0].weather_state_name });
-    // }
+    }
 
     render() {
         return (
-            <div>
-                {this.state.weather ? <div className="pb-2">Warsaw's weather for today: {this.state.weather}</div> : ""}
+            <div className="pb-2">
+                <div className="pb-2">Weather in {this.state.city}: {this.state.todaysForecast}</div>
             </div>
         );
     }
